39b0be726bf684131ed022f009034c6e874d0621
Dowon Hwang <hdwkr95.developer@gmail.com>
2023-11-01 23:44:42 +0800

    feat: LC150 Java solution

---
+++ sorting/LC150_majority_elements/Solution.java
@@ -0,0 +1,17 @@
+package sorting.LC150_majority_elements;
+import java.util.Arrays;
+
+public class Solution {
+  int majorityElement(int[] nums) {
+    Arrays.sort(nums);
+    int n = nums.length;
+    return nums[n/2];
+  }
+
+  public static void main(String[] args) {
+    int[] nums = new int[]{2,2,1,1,1,2,2};
+    Solution s = new Solution();
+    int majorityNum = s.majorityElement(nums);
+    System.out.println("majority: " + majorityNum);
+  }
+}
