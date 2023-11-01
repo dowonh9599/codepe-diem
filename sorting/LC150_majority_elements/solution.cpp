10583e071f42a221345a2a26217678f2c97bb509
Dowon Hwang <hdwkr95.developer@gmail.com>
2023-11-01 23:44:27 +0800

    fix: fix main in cpp solution

--- sorting/LC150_majority_elements/solution.cpp
+++ sorting/LC150_majority_elements/solution.cpp
@@ -4,17 +4,15 @@
 
 class Solution {
 public:
-    int majorityElement(std::vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        return nums[n/2];
-    }
+  int majorityElement(std::vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    return nums[n/2];
+  }
 };
 
 int main() {
   Solution s = Solution();
   std::vector<int> nums = {2,2,1,1,1,2,2};
-  for (int num: nums) {
-    std::cout << num << std::endl;
-  }
+    std::cout << s.majorityElement(nums) << std::endl;
 }
